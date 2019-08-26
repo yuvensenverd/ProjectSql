bcfaa2ea18ce9cbc6a5b61a6113c766d6d5a2e08
Enverd <yuvensenverd@yahoo.com>
2019-08-26 22:24:10 +0700

    confirm product

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -5,11 +5,38 @@ module.exports = {
     getProductWaiting : (req,res) =>{
         var id = req.params.id
         console.log(id)
-        var sql = `select p.name, ti.price, ti.qty, ti.productid, ti.status from product p join transactionitem ti on p.Id = ti.productid
-        join sumtransaction st on ti.transactionid = st.id where ti.status = 'Unconfirmed' AND st.userid = ${id}`
+        var sql = `select p.name, ti.price, ti.qty, ti.productid, ti.status,sh.name as shop, st.transactiondate, GROUP_CONCAT(i.imagepath) AS images from product p join image i on 
+        i.product_id = p.Id join transactionitem ti on p.Id = ti.productid join shop sh on p.shop_id = sh.userid
+        join sumtransaction st on ti.transactionid = st.id where ti.status = 'Unconfirmed' AND st.userid = ${id}
+        group by ti.id`
         db.query(sql,(err,results)=>{
             if(err) throw err;
             // console.log(results) // ARR OF OBJ
+            res.status(200).send(results)
+        })
+    },
+    getConfirmProduct : (req,res) =>{
+        var id = req.params.id
+        console.log(id)
+        var sql = `select p.name as productname, ti.id as transactionid, u.userid, ti.price, ti.qty, ti.productid, ti.status,sh.name,u.username as buyer, st.transactiondate, GROUP_CONCAT(i.imagepath) AS images from product p join image i on 
+        i.product_id = p.Id join transactionitem ti on p.Id = ti.productid join shop sh on p.shop_id = sh.userid 
+        join sumtransaction st on ti.transactionid = st.id join user u on u.userid = st.userid where ti.status = 'Unconfirmed' AND p.shop_id = ${id}
+        group by ti.id`
+        db.query(sql,(err,results)=>{
+            if(err) throw err;
+            // console.log(results) // ARR OF OBJ
+            res.status(200).send(results)
+        })
+    },
+    confirmProduct : (req,res) =>{
+        var id = req.params.id
+        var sql = `update transactionitem set status = 'Confirmed' where id = ${id}`
+        db.query(sql,(err,results)=>{
+            if(err) throw err;
+
+            console.log(sql)
+
+            console.log("Product Updated Confirmed")
             res.status(200).send(results)
         })
     }
--- routers/transactionRouter.js
+++ routers/transactionRouter.js
@@ -4,5 +4,7 @@ const {transactionController} = require('../controllers')
 const {auth} = require('../helpers/auth')
 
 router.get('/getunconfirmed/:id', auth, transactionController.getProductWaiting)
+router.get('/getunconfirmedshop/:id', auth, transactionController.getConfirmProduct)
+router.get('/confirmproduct/:id', auth, transactionController.confirmProduct)
 
 module.exports = router
