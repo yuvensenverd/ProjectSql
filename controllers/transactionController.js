c548ae2aedcf91fde128d609504c19ba0610afec
Enverd <yuvensenverd@yahoo.com>
2019-08-27 15:02:52 +0700

    notif and finish product(80%)

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -39,5 +39,43 @@ module.exports = {
             console.log("Product Updated Confirmed")
             res.status(200).send(results)
         })
+    },
+    successProduct : (req,res) =>{
+        var id = req.params.id
+        var sql = `update transactionitem set status = 'Success' where id = ${id}`
+        db.query(sql,(err,results)=>{
+            if(err) throw err;
+
+            console.log(sql)
+
+            console.log("Product Updated Success")
+            res.status(200).send(results)
+        })
+    },
+    getConfirmedProduct : (req,res) =>{
+        var id = req.params.id
+        console.log(id)
+        var sql = `select p.name, ti.price, ti.qty, ti.id as transactionid, ti.productid, ti.status,sh.name as shop, st.transactiondate, GROUP_CONCAT(i.imagepath) AS images from product p join image i on 
+        i.product_id = p.Id join transactionitem ti on p.Id = ti.productid join shop sh on p.shop_id = sh.userid
+        join sumtransaction st on ti.transactionid = st.id where ti.status = 'Confirmed' AND st.userid = ${id}
+        group by ti.id`
+        db.query(sql,(err,results)=>{
+            if(err) throw err;
+            // console.log(results) // ARR OF OBJ
+            res.status(200).send(results)
+        })
+    },
+    getNotificationLength : (req,res) =>{
+        console.log(req.params.id)
+        var sql = `select count(ti.id) as NOTIFLEN from  transactionitem ti join sumtransaction st on ti.transactionid = st.id 
+        where (ti.status = 'Confirmed' OR ti.status = 'Unconfirmed') AND st.userid = 1`
+        db.query(sql,(err,results)=>{
+            if(err) throw err;
+
+            console.log(sql)
+
+            console.log("Product Updated Success")
+            res.status(200).send(results)
+        })
     }
 }
--- routers/transactionRouter.js
+++ routers/transactionRouter.js
@@ -4,7 +4,10 @@ const {transactionController} = require('../controllers')
 const {auth} = require('../helpers/auth')
 
 router.get('/getunconfirmed/:id', auth, transactionController.getProductWaiting)
+router.get('/getconfirmed/:id', auth, transactionController.getConfirmedProduct)
 router.get('/getunconfirmedshop/:id', auth, transactionController.getConfirmProduct)
 router.get('/confirmproduct/:id', auth, transactionController.confirmProduct)
+router.get('/successproduct/:id', auth, transactionController.successProduct)
+router.get('/getnotiflen/:id', transactionController.getNotificationLength)
 
 module.exports = router
